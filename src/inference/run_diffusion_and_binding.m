5b5c3f3b68344748b62a32e2a2d7ccc67a3567ad
Magnus Röding <magnus.roding@gmail.com>
2017-02-09 12:59:13 +0100

    Simulated data-based inference routine for diffusion and binding case

--- src/inference/run_diffusion_and_binding.m
+++ src/inference/run_diffusion_and_binding.m
@@ -10,72 +10,74 @@ number_of_pixels = size(image_data_post_bleach, 1);
 number_of_post_bleach_images = 2;
 image_data_post_bleach = image_data_post_bleach(:, :, 1:number_of_post_bleach_images);
 
+delta_t = 0.250; % s.
+number_of_time_points_fine_per_coarse = 500; % dimensionless
+number_of_pad_pixels = 128;
+x_bleach = number_of_pixels / 2; % pixels
+y_bleach = number_of_pixels / 2; % pixels
+
 %% Add noise.
 sigma_noise = 0.01;
 image_data_post_bleach = image_data_post_bleach + sigma_noise * randn(size(image_data_post_bleach));
 
-%% Least squares optimization.
+%% Parameter estimation pre-work.
 
 % Set parameter bounds.
-% lb_SI = [1e-10 0 0 0.8]; % (D, kon, koff, mf)
-% ub_SI = [1e-9 10 10 1]; % (D, kon, koff, mf)
-% lb_SI = [1e-10, 0, 0, 0.8, 0.25*number_of_pixels, 0.25*number_of_pixels, 5, 0, 0]; % (D, kon, koff, mf, x_bleach, y_bleach, r_bleach_region, intensity_inside_bleach_region, intensity_outside_bleach_region)
-% ub_SI = [1e-9, 10, 10, 1, 0.75*number_of_pixels, 0.75*number_of_pixels, 0.5*number_of_pixels, 1, 1]; % (D, kon, koff, mf, x_bleach, y_bleach, r_bleach_region, intensity_inside_bleach_region, intensity_outside_bleach_region)
-lb_SI = [1e-10, 0, 0, 0.8, 5, 0, 0]; % (D, kon, koff, mf, r_bleach_region, intensity_inside_bleach_region, intensity_outside_bleach_region)
-ub_SI = [1e-9, 10, 10, 1, 0.5*number_of_pixels, 1, 1]; % (D, kon, koff, mf, r_bleach_region, intensity_inside_bleach_region, intensity_outside_bleach_region)
+lb_SI = [1e-10, 0, 0, 0.8, 5, 0, 0]; % (D, kon, koff, mobile_fraction, r_bleach, intensity_inside_bleach_region, intensity_outside_bleach_region)
+ub_SI = [1e-9, 10, 10, 1, 0.5*number_of_pixels, 1, 1]; % (D, kon, koff, mobile_fraction, r_bleach, intensity_inside_bleach_region, intensity_outside_bleach_region)
 
 lb = lb_SI;
 lb(1) = lb(1) / pixel_size^2;
-
 ub = ub_SI;
 ub(1) = ub(1) / pixel_size^2;
 
 % Initial guess
-% param_guess_SI = [2e-10 0.5 0.5 1]; % (D, kon, koff, mf)
-% param_guess_SI = [2.5e-10, 1.0, 1.0, 0.9, 0.5*number_of_pixels, 0.5*number_of_pixels, 30, 0.6, 0.9]; % (D, kon, koff, mf, x_bleach, y_bleach, r_bleach_region, intensity_inside_bleach_region, intensity_outside_bleach_region)
 param_guess_SI = [2.5e-10, 1.0, 1.0, 0.9, 32, 0.6, 0.9]; % (D, kon, koff, mf, r_bleach_region, intensity_inside_bleach_region, intensity_outside_bleach_region)
 param_guess = param_guess_SI;
 param_guess(1) = param_guess(1) / pixel_size^2;
 
-% Optimization routine options.
-options = optimoptions(@lsqnonlin);
-options.Algorithm = 'levenberg-marquardt';%'trust-region-reflective';
-options.Display = 'iter';
-options.TolFun = 1e-6;
-options.TolCon = 1e-6;
-options.TolX = 1e-6;
-options.UseParallel = true;
+%% Least-squares optimization.
 
-% Function handle.
-% fun = @(param)residual(param(1), param(2), param(3), param(4), x_bleach, y_bleach, r_bleach_region, intensity_inside_bleach_region, intensity_outside_bleach_region, delta_t, image_data_post_bleach);
-% fun = @(param)residual(param(1), param(2), param(3), param(4), param(5), param(6), param(7), param(8), param(9), delta_t, image_data_post_bleach);
-fun = @(param)residual(param(1), param(2), param(3), param(4), x_bleach, y_bleach, param(5), param(6), param(7), delta_t, image_data_post_bleach);
-    
+% fun = @(param)residual_diffusion_and_binding(   param(1), ...
+%                                                 param(2), ...
+%                                                 param(3), ...
+%                                                 param(4), ...
+%                                                 x_bleach, ...
+%                                                 y_bleach, ...
+%                                                 param(5), ...
+%                                                 param(6), ...
+%                                                 param(7), ...
+%                                                 delta_t, ...
+%                                                 number_of_time_points_fine_per_coarse, ...
+%                                                 number_of_pad_pixels, ...
+%                                                 image_data_post_bleach);
+% options = optimoptions(@lsqnonlin);
+% options.Algorithm = 'levenberg-marquardt';%'trust-region-reflective';
+% options.Display = 'iter';
+% options.TolFun = 1e-6;
+% options.TolCon = 1e-6;
+% options.TolX = 1e-6;
+% options.UseParallel = true;
+% 
 % [param_hat, ss] = lsqnonlin(fun, param_guess, lb, ub, options);
-[param_hat, ss] = lsqnonlin(fun, param_guess, [], [], options);
 
 %% Pattern-search optimization.
-
-lb_SI = [2.25e-10, 0.75, 0.75, 0.8, 24, 0.55, 0.8]; % (D, kon, koff, mf, r_bleach_region, intensity_inside_bleach_region, intensity_outside_bleach_region)
-ub_SI = [2.75e-10, 1.2, 1.25, 1, 40, 0.70, 1]; % (D, kon, koff, mf, r_bleach_region, intensity_inside_bleach_region, intensity_outside_bleach_region)
-lb = lb_SI;
-lb(1) = lb(1) / pixel_size^2;
-ub = ub_SI;
-ub(1) = ub(1) / pixel_size^2;
-param_guess_SI = [2.5e-10, 1.0, 1.0, 0.9, 32, 0.6, 0.9]; % (D, kon, koff, mf, r_bleach_region, intensity_inside_bleach_region, intensity_outside_bleach_region)
-param_guess = param_guess_SI;
-param_guess(1) = param_guess(1) / pixel_size^2;
-
+fun = @(param)sum ( residual_diffusion_and_binding( param(1), ...
+                                                    param(2), ...
+                                                    param(3), ...
+                                                    param(4), ...
+                                                    x_bleach, ...
+                                                    y_bleach, ...
+                                                    param(5), ...
+                                                    param(6), ...
+                                                    param(7), ...
+                                                    delta_t, ...
+                                                    number_of_time_points_fine_per_coarse, ...
+                                                    number_of_pad_pixels, ...
+                                                    image_data_post_bleach).^2 );
 options = psoptimset(@patternsearch);
-options.Algorithm = 'levenberg-marquardt';%'trust-region-reflective';
 options.Display = 'iter';
-options.CheckGradients = false;
-options.SpecifyObjectiveGradient = false;
-options.FunctionTolerance = 1e-6;
-options.StepTolerance = 1e-6;
-options.OptimalityTolerance = 1e-6;
-
-fun = @(param)sum( residual(param(1), param(2), param(3), param(4), x_bleach, y_bleach, param(5), param(6), param(7), delta_t, image_data_post_bleach).^2 );
+options.UseParallel = true;
 
 [param_hat, ss] = patternsearch(fun, param_guess, [], [], [], [], lb, ub, [], options);
 
--- src/inference/test_signal_diffusion_and_binding.m
+++ src/inference/test_signal_diffusion_and_binding.m
@@ -7,21 +7,20 @@ close all hidden
 D_SI = 2.5e-10; % m^2/s
 pixel_size = 7.5e-07; % m
 D = D_SI / pixel_size^2; % pixels^2 / s
-k_on = 0%1; % 1/s
-k_off = inf%1; % 1/s
+k_on = 1; % 1/s
+k_off = 1; % 1/s
 mobile_fraction = 0.90; % dimensionless
 
 delta_t = 0.250; % s.
 number_of_time_points_fine_per_coarse = 500; % dimensionless
 number_of_pixels = 256;
-number_of_post_bleach_images = 10;
+number_of_post_bleach_images = 20;
 number_of_pad_pixels = 128;
 x_bleach = number_of_pixels / 2; % pixels
 y_bleach = number_of_pixels / 2; % pixels
 r_bleach = 32; % pixels
 intensity_inside_bleach_region = 0.6; % a.u.
 intensity_outside_bleach_region = 0.85; % a.u.
-
 
 %% Simulate.
 image_data_post_bleach = signal_diffusion_and_binding(  D, ...
@@ -44,7 +43,7 @@ sigma_noise = 0.0;
 image_data_post_bleach = image_data_post_bleach + sigma_noise * randn(size(image_data_post_bleach));
 
 %% Save data.
-% save('simulated_data_zero_noise.mat', 'image_data_post_bleach', 'delta_t', 'pixel_size', 'D_SI', 'D', 'k_on', 'k_off', 'mobile_fraction', 'x_bleach', 'y_bleach', 'r_bleach_region', 'intensity_inside_bleach_region', 'intensity_outside_bleach_region');
+save('simulated_data_zero_noise.mat', 'image_data_post_bleach', 'delta_t', 'pixel_size');
 
 %% Plot.
 result_pde = [];
@@ -56,17 +55,17 @@ imagesc(result_pde)
 axis 'equal'
 axis([0 number_of_post_bleach_images*number_of_pixels 0 number_of_pixels])
 axis off
-
-[X, Y] = meshgrid(1:number_of_pixels, 1:number_of_pixels);
-X = X - 0.5;
-Y = Y - 0.5;
-ind = find( (X - x_bleach).^2 + (Y - y_bleach).^2 <= r_bleach^2 );
-ind = ind(:);
-recovery_curve = zeros(1, number_of_post_bleach_images);
-for current_image_post_bleach = 1:number_of_post_bleach_images
-    slice = image_data_post_bleach(:, :, current_image_post_bleach);
-    recovery_curve(current_image_post_bleach) = mean(slice(ind));
-end
-figure
-plot(delta_t:delta_t:number_of_post_bleach_images*delta_t, recovery_curve)
-
+% 
+% [X, Y] = meshgrid(1:number_of_pixels, 1:number_of_pixels);
+% X = X - 0.5;
+% Y = Y - 0.5;
+% ind = find( (X - x_bleach).^2 + (Y - y_bleach).^2 <= r_bleach^2 );
+% ind = ind(:);
+% recovery_curve = zeros(1, number_of_post_bleach_images);
+% for current_image_post_bleach = 1:number_of_post_bleach_images
+%     slice = image_data_post_bleach(:, :, current_image_post_bleach);
+%     recovery_curve(current_image_post_bleach) = mean(slice(ind));
+% end
+% figure
+% plot(delta_t:delta_t:number_of_post_bleach_images*delta_t, recovery_curve)
+% 
