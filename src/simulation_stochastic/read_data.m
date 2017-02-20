f50d410a5ecfae60e920ef238809802b11dd0526
Magnus Röding <magnus.roding@gmail.com>
2017-02-20 12:27:59 +0100

    Reduction operator

--- src/simulation_stochastic/read_data.m
+++ src/simulation_stochastic/read_data.m
@@ -21,4 +21,6 @@ axis 'equal'
 axis([0 number_of_post_bleach_images*number_of_pixels 0 number_of_pixels])
 axis off
 
-sum(image_data_post_bleach(:))
+max(image_data_post_bleach(:))
+min(image_data_post_bleach(:))
+mean(image_data_post_bleach(:))
