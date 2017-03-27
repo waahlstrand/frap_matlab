b6388701dbb9df38b5d510e6225b35ce84028999
Magnus Röding <magnus.roding@gmail.com>
2017-03-27 17:14:34 +0200

    Converted working 1D solver to 2D case

--- src/signal_pde/experimental_code/simulate_pde_spectral.m
+++ src/signal_pde/experimental_code/simulate_pde_spectral.m
@@ -7,7 +7,7 @@ close all hidden
 delta_t = 0.25; % s
 number_of_post_bleach_images = 5;
 number_of_pixels = 256;
-number_of_pad_pixels = 0;
+number_of_pad_pixels = 128;
 r_bleach_region = 32; % pixels
 
 intensity_inside_bleach_region = 0.6;
@@ -21,13 +21,11 @@ k_off = 1.0; % 1/s
 p_free = k_off / ( k_on + k_off );
 p_bound = k_on / ( k_on + k_off );
 
-%% Generate post bleach image data
-
-% Initial condition. Create a high resolution initial condition which is 
-% then downsampled to avoid too sharp edges.
+%% Initial condition. 
+% Create a high resolution initial condition which is then downsampled to 
+% avoid too sharp edges.
 
 upsampling_factor = 3;
-
 
 [X, Y] = meshgrid(1:upsampling_factor*(number_of_pixels + 2 * number_of_pad_pixels), 1:upsampling_factor*(number_of_pixels + 2 * number_of_pad_pixels));
 X = X - 0.5;
@@ -47,8 +45,6 @@ U0 = p_free * U0;
 B0 = imresize(B0, [number_of_pixels + 2 * number_of_pad_pixels, number_of_pixels + 2 * number_of_pad_pixels]);
 U0 = imresize(U0, [number_of_pixels + 2 * number_of_pad_pixels, number_of_pixels + 2 * number_of_pad_pixels]);
 
-% figure, imagesc(U0(number_of_pad_pixels+1:end-number_of_pad_pixels, number_of_pad_pixels+1:end-number_of_pad_pixels)), axis 'equal'
-
 clear X Y
 
 %% FFT of initial conditions.
@@ -61,48 +57,23 @@ F_B0 = fft2(B0);
 
 %% FFT space time evolution of PDE system.
 
-F_image_data_post_bleach_u = zeros(number_of_pixels, number_of_pixels, number_of_post_bleach_images);
-F_image_data_post_bleach_b = zeros(number_of_pixels, number_of_pixels, number_of_post_bleach_images);
-image_data_post_bleach_u = zeros(number_of_pixels, number_of_pixels, number_of_post_bleach_images);
-image_data_post_bleach_b = zeros(number_of_pixels, number_of_pixels, number_of_post_bleach_images);
+F_image_data_post_bleach_u = zeros(number_of_pixels + 2 * number_of_pad_pixels, number_of_pixels + 2 * number_of_pad_pixels, number_of_post_bleach_images);
+F_image_data_post_bleach_b = zeros(number_of_pixels + 2 * number_of_pad_pixels, number_of_pixels + 2 * number_of_pad_pixels, number_of_post_bleach_images);
+image_data_post_bleach_u = zeros(number_of_pixels + 2 * number_of_pad_pixels, number_of_pixels + 2 * number_of_pad_pixels, number_of_post_bleach_images);
+image_data_post_bleach_b = zeros(number_of_pixels + 2 * number_of_pad_pixels, number_of_pixels + 2 * number_of_pad_pixels, number_of_post_bleach_images);
 
-% [XSI1, XSI2] = meshgrid(-number_of_pixels/2:number_of_pixels/2-1, -number_of_pixels/2:number_of_pixels/2-1);
-% XSISQ = XSI1.^2 + XSI2.^2;
-[XSI1, XSI2] = meshgrid(0:number_of_pixels-1, 0:number_of_pixels-1);
+[XSI1, XSI2] = meshgrid(-(number_of_pixels + 2 * number_of_pad_pixels)/2:(number_of_pixels + 2 * number_of_pad_pixels)/2-1, ...
+                        -(number_of_pixels + 2 * number_of_pad_pixels)/2:(number_of_pixels + 2 * number_of_pad_pixels)/2-1);
 XSISQ = XSI1.^2 + XSI2.^2;
 
-% For single point.
-% i = 1;
-% j = 1;
-% F_U = zeros(number_of_post_bleach_images, 1);
-% F_B = zeros(number_of_post_bleach_images, 1);
 T = delta_t * (1:number_of_post_bleach_images);
-% 
-% for i = 1:number_of_post_bleach_images
-%     F_image_data_post_bleach_u(:, :, i) = exp( ( - D * XSISQ - k_on) * T(i) ) .* F_U0 + exp( k_off * T(i) ) .* F_B0;
-%     F_image_data_post_bleach_b(:, :, i) = exp( k_on * T(i) ) .* F_U0 + exp( - k_off * T(i) ) .* F_B0;
-% %     F_image_data_post_bleach_u(:, :, i) = exp( ( - D * XSISQ - k_on) .* T(i) .* F_U0 ) + exp( k_off * T(i) * F_B0 ); 
-% %     F_image_data_post_bleach_b(:, :, i) = exp( k_on * T(i) * F_U0 ) + exp( - k_off * T(i) * F_B0 ); 
-% 
-%     image_data_post_bleach_u(:, :, i) = ifftshift(F_image_data_post_bleach_u(:, :, i));
-%     image_data_post_bleach_u(:, :, i) = ifft2(image_data_post_bleach_u(:, :, i));
-%     image_data_post_bleach_b(:, :, i) = ifftshift(F_image_data_post_bleach_b(:, :, i));
-%     image_data_post_bleach_b(:, :, i) = ifft2(image_data_post_bleach_b(:, :, i));
-% end
-% 
-% FRAP = abs(image_data_post_bleach_u) + abs(image_data_post_bleach_b);
-% % figure, imagesc(abs(image_data_post_bleach_u(:,:,i))+abs(image_data_post_bleach_b(:,:,i)))
-% 
-% imagesc(reshape(FRAP, [number_of_pixels, number_of_pixels*number_of_post_bleach_images]))
-% axis 'equal'
-
 
 for t = 1:number_of_post_bleach_images
-    for i = 1:number_of_pixels
+    for i = 1:(number_of_pixels + 2 * number_of_pad_pixels)
         disp([t, i])
-        for j = 1:number_of_pixels
+        for j = 1:(number_of_pixels + 2 * number_of_pad_pixels)
             A = [- D * XSISQ(i, j) - k_on, k_off ; k_on, - k_off];
-            c_vector_hat = exp( A * T(t) ) * [F_U0(i, j) ; F_B0(i, j)];
+            c_vector_hat = expm( A * T(t) ) * [F_U0(i, j) ; F_B0(i, j)];
             
             F_image_data_post_bleach_u(i, j, t) = c_vector_hat(1);
             F_image_data_post_bleach_b(i, j, t) = c_vector_hat(2);
@@ -110,23 +81,15 @@ for t = 1:number_of_post_bleach_images
     end
 end
 
-% F_FRAP = abs(F_image_data_post_bleach_u) + abs(F_image_data_post_bleach_b);
-% imagesc(reshape(log10(F_FRAP), [number_of_pixels, number_of_pixels*number_of_post_bleach_images]))
-% axis 'equal'
-
-for i = 1:number_of_post_bleach_images
-%     image_data_post_bleach_u(:, :, i) = ifftshift(F_image_data_post_bleach_u(:, :, i));
-    image_data_post_bleach_u(:, :, i) = abs(ifft2(F_image_data_post_bleach_u(:, :, i)));
-%     image_data_post_bleach_b(:, :, i) = ifftshift(F_image_data_post_bleach_b(:, :, i));
-    image_data_post_bleach_b(:, :, i) = abs(ifft2(F_image_data_post_bleach_b(:, :, i)));
+for t = 1:number_of_post_bleach_images
+    image_data_post_bleach_u(:, :, t) = abs(ifft2(ifftshift2(F_image_data_post_bleach_u(:, :, t))));
+    image_data_post_bleach_b(:, :, t) = abs(ifft2(ifftshift2(F_image_data_post_bleach_b(:, :, t))));
 end
 
+image_data_post_bleach_u = image_data_post_bleach_u(number_of_pad_pixels+1:end-number_of_pad_pixels, :);
+image_data_post_bleach_b = image_data_post_bleach_b(number_of_pad_pixels+1:end-number_of_pad_pixels, :);
+
 FRAP = image_data_post_bleach_u + image_data_post_bleach_b;
-% figure, imagesc(abs(image_data_post_bleach_u(:,:,i))+abs(image_data_post_bleach_b(:,:,i)))
 
 imagesc(reshape(FRAP, [number_of_pixels, number_of_pixels*number_of_post_bleach_images]))
 axis 'equal'
-
-
-
-% 
