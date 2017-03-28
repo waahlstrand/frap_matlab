37d5e82ad36738cdea998be3c82dab0db49ea8b6
Magnus Röding <magnus.roding@gmail.com>
2017-03-28 16:29:22 +0200

    Refined comparison routine

--- src/run_sim_study_compare_px_rc.m
+++ src/run_sim_study_compare_px_rc.m
@@ -5,7 +5,6 @@ close all hidden
 
 addpath('signal_pde');
 
-
 %% Init parallel pool.
 delete(gcp('nocreate'))
 c = parcluster('local');
@@ -16,7 +15,7 @@ parpool(c, c.NumWorkers);
 pixel_size = 7.598e-07; % m
 delta_t = 0.2650; % s
 number_of_pixels = 256;
-number_of_images = 2%100;
+number_of_images = 100
 number_of_pad_pixels = 128;
 
 D_SI = 2.5e-10; % m^2/s
@@ -55,11 +54,12 @@ ub_Iu = 1.0;
 lb = [lb_D, lb_k_on, lb_k_off, lb_mf, lb_Ib, lb_Iu]; 
 ub = [ub_D, ub_k_on, ub_k_off, ub_mf, ub_Ib, ub_Iu]; 
 
-param_guess = [D, k_on, k_off, mf, Ib, Iu];
+param_true = [D, k_on, k_off, mf, Ib, Iu];
+param_guess = param_true;
 number_of_fits = 1;
 
 % Loop forever in parallel.
-parfor i = 1:1000
+parfor i = 1:100000
     random_seed = sum( 1e6 * clock() );
     random_stream = RandStream('mt19937ar', 'Seed', random_seed);
     RandStream.setGlobalStream(random_stream);
@@ -78,11 +78,8 @@ parfor i = 1:1000
                         number_of_images, ...
                         number_of_pad_pixels);
 
-    sigma_noise = 0.025;
+    sigma_noise = randsample([0.001 0.002 0.005 0.01 0.02 0.05], 1);
     data = data + sigma_noise * randn(size(data));
-
-
-
 
     [param_hat_px, ss_px] = estimate_db_px( data, ...
                                             x_bleach, ...
@@ -111,9 +108,10 @@ parfor i = 1:1000
                                             number_of_fits);
 
     mat_file = matfile(['est_' num2str(random_seed) '.mat'], 'writable', true);
+    mat_file.param_true = param_true;
+    mat_file.sigma_noise = sigma_noise;
     mat_file.param_hat_px = param_hat_px;
     mat_file.ss_px = ss_px;
     mat_file.param_hat_rc = param_hat_rc;
     mat_file.ss_rc = ss_rc;
-%     save(['est_' num2str(random_seed) '.mat'], 'param_hat_px', 'ss_px', 'param_hat_rc', 'ss_rc')
 end
