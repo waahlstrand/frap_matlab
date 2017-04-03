36d3e2ebda5b67cdd536bc0e8cd670fc2a62c0aa
Magnus Röding <magnus.roding@gmail.com>
2017-04-03 08:42:02 +0200

    Changed input parameters to stochastic simulation

--- src/signal_stochastic/simulate_diffusion_and_binding.jl
+++ src/signal_stochastic/simulate_diffusion_and_binding.jl
@@ -2,7 +2,7 @@ workspace()
 
 @everywhere include("simulate.jl")
 
-function simulate_diffusion_and_binding()
+function simulate_diffusion_and_binding(D_SI::Float64, k_on::Float64, k_off::Float64)
 	# Inititalization of random number generation device.
 	random_seed::Int64 = convert(Int64, time_ns())
 	srand(random_seed)
@@ -14,20 +14,20 @@ function simulate_diffusion_and_binding()
 	delta_t::Float64 = 0.2650 # s
 	number_of_post_bleach_images::Int64 = 10#4#40
 	number_of_pixels::Int64 = 256 #  pixels
-	r_bleach::Float64 = 32.0 #  pixels
+	r_bleach::Float64 = 15e-6 / pixel_size # pixels corresponding to 15 µm radius (30 µm diameter)
 	intensity_inside_bleach_region::Float64 = 0.6
 	intensity_outside_bleach_region::Float64 = 0.9
 	pixel_size::Float64 = 7.598e-7 # m
 
 	# Particle parameters.
-	D_SI::Float64 = 5.0e-10 # m^2 / s
+	#D_SI::Float64 = 5.0e-10 # m^2 / s
 	D::Float64 = D_SI / pixel_size^2 # pixels^2 / s
-	k_on::Float64 = 0.5 # 1/s
-	k_off::Float64 = 1.0#1.0 # 1/s
-	mobile_fraction::Float64 = 0.9
+	#k_on::Float64 = 0.5 # 1/s
+	#k_off::Float64 = 1.0#1.0 # 1/s
+	mobile_fraction::Float64 = 1.0
 
 	# Simulation parameters.
-	number_of_pad_pixels::Int64 = 128#256 # pixels
+	number_of_pad_pixels::Int64 = 128 # pixels
 	
 	number_of_time_steps_fine_per_course::Int64 = 64
 	
@@ -52,7 +52,7 @@ function simulate_diffusion_and_binding()
 	end
 
 	# Save output.
-	file_name_output::String = "simulated_frap_data.dat"
+	file_name_output::String = join(("simulated_frap_data_", str(D), "_", str(k_on), "_", str(k_off), ".dat"))
 	file_stream_output::IOStream = open(file_name_output, "w")
 	write(file_stream_output, D)
 	write(file_stream_output, k_on)
