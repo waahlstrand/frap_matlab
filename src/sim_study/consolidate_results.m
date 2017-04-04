18c3c509e1281ab0947230a16ac7b4a6f5fab2d7
Magnus Röding <magnus.roding@gmail.com>
2017-04-04 09:14:46 +0200

    Changes

--- src/sim_study/consolidate_results.m
+++ src/sim_study/consolidate_results.m
@@ -2,31 +2,43 @@ clear
 clc
 close all hidden
 
-files = dir('results_20170331\*.mat');
+folder = 'results_20170403';
+files = dir([folder '\*.mat']);
 number_of_files = numel(files);
 
-param_true = [];
-param_hat_px = [];
-param_hat_rc = [];
-sigma_noise = [];
+param_true = nan(number_of_files, 6);
+param_hat_px = nan(number_of_files, 6);
+param_hat_rc = nan(number_of_files, 6);
+sigma_noise = nan(number_of_files, 1);
+
+ok = true(number_of_files, 1);
 
 for current_file = 1:number_of_files
-
-    file_path = ['results_20170331\' files(current_file).name];
-%     try
+    %if mod(current_file, 100) == 0
+        disp(current_file)
+    %end
+    
+    file_path = [folder '\' files(current_file).name];
+    
+    try
         file_data = load(file_path);
 
-        param_true = [param_true ; file_data.param_true];
-        param_hat_px = [param_hat_px ; file_data.param_hat_px];
-        param_hat_rc = [param_hat_rc ; file_data.param_hat_rc];
-        sigma_noise = [sigma_noise ; file_data.sigma_noise];
-%     catch
-%         disp(current_file)
-%         delete(file_path)
-%     end
+        param_true(current_file, :) = file_data.param_true;
+        param_hat_px(current_file, :) = file_data.param_hat_px;
+        param_hat_rc(current_file, :) = file_data.param_hat_rc;
+        sigma_noise(current_file, :) = file_data.sigma_noise;
+    catch
+        disp(['Deleting file # ' num2str(current_file)])
+        ok(current_file) = false;
+        delete(file_path)
+    end
 end
+param_true = param_true(ok, :);
+param_hat_px = param_hat_px(ok, :);
+param_hat_rc = param_hat_rc(ok, :);
+sigma_noise = sigma_noise(ok);
 
-clear files number_of_files current_file file_path file_data
+clear files number_of_files current_file file_path file_data ok
 
 save('results.mat')
     
