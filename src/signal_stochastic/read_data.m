3f59df7d83278017c8476bbf3d35f9b231f176e4
Magnus Röding <magnus.roding@gmail.com>
2017-04-05 19:23:23 +0200

    Changed to consistent notation

--- src/signal_stochastic/read_data.m
+++ src/signal_stochastic/read_data.m
@@ -4,23 +4,22 @@ clc
 close all hidden
 
 %% Read file.
-file_id = fopen('simulated_frap_data_5.0e-10_1.0_10.0.dat');
+file_id = fopen('simulated_stochastic_data_1.0e-10_5.0_5.0.dat');
 
 D = fread(file_id, 1, 'float64');
 k_on = fread(file_id, 1, 'float64');
 k_off = fread(file_id, 1, 'float64');
-mobile_fraction = fread(file_id, 1, 'float64');
-delta_t = fread(file_id, 1, 'float64');
+mf = fread(file_id, 1, 'float64');
+Ib = fread(file_id, 1, 'float64');
+Iu = fread(file_id, 1, 'float64');
 r_bleach = fread(file_id, 1, 'float64');
-pixel_size = fread(file_id, 1, 'float64');
 number_of_pixels = fread(file_id, 1, 'int64');
-number_of_post_bleach_images = fread(file_id, 1, 'int64');
+number_of_images = fread(file_id, 1, 'int64');
 number_of_pad_pixels = fread(file_id, 1, 'int64');
-intensity_inside_bleach_region = fread(file_id, 1, 'float64');
-intensity_outside_bleach_region = fread(file_id, 1, 'float64');
+delta_t = fread(file_id, 1, 'float64');
+pixel_size = fread(file_id, 1, 'float64');
 number_of_particles = fread(file_id, 1, 'int64');
-
-image_data_post_bleach = reshape(fread(file_id, 'int64'), [number_of_pixels, number_of_pixels, number_of_post_bleach_images]);
+data = reshape(fread(file_id, 'int64'), [number_of_pixels, number_of_pixels, number_of_images]);
 
 fclose(file_id);
 
@@ -30,32 +29,32 @@ clear file_id
 save('simulated_frap_data.mat')
 
 %% Plot data.
-k = 1 / (intensity_inside_bleach_region*pi*r_bleach^2 + intensity_outside_bleach_region * ((number_of_pixels+2*number_of_pad_pixels)^2 - pi*r_bleach^2));
-%k*intensity_inside_bleach_region*pi*r_bleach^2+k*intensity_outside_bleach_region*((number_of_pixels+2*number_of_pad_pixels)^2-pi*r_bleach^2)
-%expected = number_of_particles * intensity_outside_bleach_region * k;
-image_data_post_bleach = image_data_post_bleach / (number_of_particles * k);
+k = 1 / (Ib*pi*r_bleach^2 + Iu * ((number_of_pixels+2*number_of_pad_pixels)^2 - pi*r_bleach^2));
+%k*Ib*pi*r_bleach^2+k*Iu*((number_of_pixels+2*number_of_pad_pixels)^2-pi*r_bleach^2)
+%expected = number_of_particles * Iu * k;
+data = data / (number_of_particles * k);
 
 %M = load('../inference/simulated_data_diffusion_and_binding.mat');
 
 figure
 hold on
-%imagesc([reshape(M.image_data_post_bleach, [number_of_pixels, number_of_pixels*number_of_post_bleach_images]) ; reshape(image_data_post_bleach-M.image_data_post_bleach, [number_of_pixels, number_of_pixels*number_of_post_bleach_images])]);
-%imagesc(reshape(image_data_post_bleach-M.image_data_post_bleach, [number_of_pixels, number_of_pixels*number_of_post_bleach_images]));
-imagesc(reshape(image_data_post_bleach, [number_of_pixels, number_of_pixels*number_of_post_bleach_images]));
+%imagesc([reshape(M.data, [number_of_pixels, number_of_pixels*number_of_images]) ; reshape(data-M.data, [number_of_pixels, number_of_pixels*number_of_images])]);
+%imagesc(reshape(data-M.data, [number_of_pixels, number_of_pixels*number_of_images]));
+imagesc(reshape(data, [number_of_pixels, number_of_pixels*number_of_images]));
 axis 'equal'
-axis([0 number_of_post_bleach_images*number_of_pixels 0 number_of_pixels])
+axis([0 number_of_images*number_of_pixels 0 number_of_pixels])
 axis off
 hold off
 
 %figure
 %hold on
-%hist(image_data_post_bleach(:) - M.image_data_post_bleach(:), 100)
+%hist(data(:) - M.data(:), 100)
 %hold off
 
-%sum(M.image_data_post_bleach(:))
-%sum(image_data_post_bleach(:))
-%std(image_data_post_bleach(:) - M.image_data_post_bleach(:))
+%sum(M.data(:))
+%sum(data(:))
+%std(data(:) - M.data(:))
 
-%max(image_data_post_bleach(:))
-%min(image_data_post_bleach(:))
-% mean(image_data_post_bleach(:)) / mean(image_data_post_bleach(:,1,1))
+%max(data(:))
+%min(data(:))
+% mean(data(:)) / mean(data(:,1,1))
