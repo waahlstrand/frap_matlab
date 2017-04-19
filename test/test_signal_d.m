7e8d4878ff00d9bdc5d5e42832119ca3dd2e0d51
Magnus Röding <magnus.roding@gmail.com>
2017-04-19 15:25:15 +0200

    Cleaned up test routines

--- test/test_signal_d.m
+++ test/test_signal_d.m
@@ -3,42 +3,51 @@ clear
 clc
 close all hidden
 
+addpath('../src/signal');
+
 %% Parameters.
-D_SI = 2.5e-10; % m^2/s
-pixel_size = 7.598e-07; % m
+D_SI = 1e-11; % m^2/s
+pixel_size = 7.5e-07; % m
 D = D_SI / pixel_size^2; % pixels^2 / s
 mf = 1.0; % dimensionless
 
-delta_t = 0.2650; % s
+delta_t = 0.2; % s
 number_of_pixels = 256;
-number_of_images = 10;
+number_of_images = 5;
 number_of_pad_pixels = 128;
 Ib = 0.6; % a.u.
-Iu = 0.9; % a.u.
+Iu = 1.0;%0.9; % a.u.
 x_bleach = 128; % pixels
 y_bleach = 128; % pixels
-r_bleach = 32; % pixels
+r_bleach = 25e-6 / pixel_size; % pixels corresponding to 15 µm radius (30 µm diameter)
+lx_bleach = r_bleach;
+ly_bleach = r_bleach;
+
+% param_bleach = [x_bleach, y_bleach, r_bleach]; % Circular.
+param_bleach = [x_bleach, y_bleach, lx_bleach, ly_bleach]; % Rectangular.
 
 %% Simulate.
 tic
-signal = signal_d(  D, ...
-                    mf, ...
-                    Ib, ...
-                    Iu, ...
-                    x_bleach, ...
-                    y_bleach, ...
-                    r_bleach, ...
-                    delta_t, ...
-                    number_of_pixels, ...
-                    number_of_images, ...
-                    number_of_pad_pixels);
+data = signal_d( ...
+    D, ...
+    mf, ...
+    Ib, ...
+    Iu, ...
+    param_bleach, ...
+    delta_t, ...
+    number_of_pixels, ...
+    number_of_images, ...
+    number_of_pad_pixels);
 toc
+
+sigma_noise = 0.005;
+data = data + sigma_noise * randn(size(data));
 
 %% Plot solution.
 figure
 hold on
-imagesc(reshape(signal, [number_of_pixels, number_of_pixels * number_of_images]))
-axis 'equal'
+imagesc(reshape(data, [number_of_pixels, number_of_pixels * number_of_images]))
+% axis 'equal'
 axis([0 number_of_images*number_of_pixels 0 number_of_pixels])
 axis off
 hold off
@@ -47,16 +56,18 @@ hold off
 [X, Y] = meshgrid(1:number_of_pixels, 1:number_of_pixels);
 X = X - 0.5;
 Y = Y - 0.5;
-ind = find( (X - x_bleach).^2 + (Y - y_bleach).^2 <= r_bleach^2 );
+
+if numel(param_bleach) == 3 % Circular.
+    ind = find( (X - x_bleach).^2 + (Y - y_bleach).^2 <= r_bleach^2 );
+else % Rectangular.
+    ind = find( X >= x_bleach - 0.5 * lx_bleach & X <= x_bleach + 0.5 * lx_bleach & Y >= y_bleach - 0.5 * ly_bleach & Y <= y_bleach + 0.5 * ly_bleach );
+end
 ind = ind(:);
+
 recovery_curve = zeros(1, number_of_images);
 for current_image = 1:number_of_images
-    slice = signal(:, :, current_image);
+    slice = data(:, :, current_image);
     recovery_curve(current_image) = mean(slice(ind));
 end
 figure
 plot(delta_t:delta_t:number_of_images*delta_t, recovery_curve)
-
-%% Save data.
-% clear current_image X Y ind recovery_curve slice
-% save('simulated_signal_d.mat')
--- test/test_signal_db.m
+++ test/test_signal_db.m
@@ -3,8 +3,10 @@ clear
 clc
 close all hidden
 
+addpath('../src/signal');
+
 %% Parameters.
-D_SI = 5e-11; % m^2/s
+D_SI = 1e-11; % m^2/s
 pixel_size = 7.5e-07; % m
 D = D_SI / pixel_size^2; % pixels^2 / s
 k_on = 0.5; % 1/s
@@ -13,32 +15,36 @@ mf = 1.0; % dimensionless
 
 delta_t = 0.2; % s
 number_of_pixels = 256;
-number_of_images = 100;
+number_of_images = 5;
 number_of_pad_pixels = 128;
 Ib = 0.6; % a.u.
 Iu = 1.0;%0.9; % a.u.
 x_bleach = 128; % pixels
 y_bleach = 128; % pixels
-r_bleach = 15e-6 / pixel_size; % pixels corresponding to 15 µm radius (30 µm diameter)
+r_bleach = 25e-6 / pixel_size; % pixels corresponding to 15 µm radius (30 µm diameter)
+lx_bleach = r_bleach;
+ly_bleach = r_bleach;
+
+% param_bleach = [x_bleach, y_bleach, r_bleach]; % Circular.
+param_bleach = [x_bleach, y_bleach, lx_bleach, ly_bleach]; % Rectangular.
 
 %% Simulate.
 tic
-data = signal_db( D, ...
-                    k_on, ...
-                    k_off, ...
-                    mf, ...
-                    Ib, ...
-                    Iu, ...
-                    x_bleach, ...
-                    y_bleach, ...
-                    r_bleach, ...
-                    delta_t, ...
-                    number_of_pixels, ...
-                    number_of_images, ...
-                    number_of_pad_pixels);
+data = signal_db( ...
+    D, ...
+    k_on, ...
+    k_off, ...
+    mf, ...
+    Ib, ...
+    Iu, ...
+    param_bleach, ...
+    delta_t, ...
+    number_of_pixels, ...
+    number_of_images, ...
+    number_of_pad_pixels);
 toc
 
-sigma_noise = 0.5;
+sigma_noise = 0.005;
 data = data + sigma_noise * randn(size(data));
 
 %% Plot solution.
@@ -54,8 +60,14 @@ hold off
 [X, Y] = meshgrid(1:number_of_pixels, 1:number_of_pixels);
 X = X - 0.5;
 Y = Y - 0.5;
-ind = find( (X - x_bleach).^2 + (Y - y_bleach).^2 <= r_bleach^2 );
+
+if numel(param_bleach) == 3 % Circular.
+    ind = find( (X - x_bleach).^2 + (Y - y_bleach).^2 <= r_bleach^2 );
+else % Rectangular.
+    ind = find( X >= x_bleach - 0.5 * lx_bleach & X <= x_bleach + 0.5 * lx_bleach & Y >= y_bleach - 0.5 * ly_bleach & Y <= y_bleach + 0.5 * ly_bleach );
+end
 ind = ind(:);
+
 recovery_curve = zeros(1, number_of_images);
 for current_image = 1:number_of_images
     slice = data(:, :, current_image);
@@ -63,7 +75,3 @@ for current_image = 1:number_of_images
 end
 figure
 plot(delta_t:delta_t:number_of_images*delta_t, recovery_curve)
-
-%% Save data.
-% clear current_image X Y ind recovery_curve slice
-% save('simulated_signal_db.mat')
