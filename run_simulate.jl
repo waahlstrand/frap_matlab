6242249034a05af1bb82f866c68f974a8d1190ed
Stb-Lex <stb.lex.boussole@gmail.com>
2018-09-12 09:06:27 +0200

    Changes to stochastic simulation and validation

--- run_simulate.jl
+++ run_simulate.jl
@@ -9,7 +9,15 @@ function run_simulate(	D_SI::Float64,
 						alpha::Float64,
 						beta::Float64,
 						gamma::Float64,
-						number_of_bleach_frames::Int64)
+						bleach_region_shape::Int64,
+						r_bleach::Float64,
+						lx_bleach::Float64,
+						ly_bleach::Float64,
+						pixel_size::Float64,
+						number_of_prebleach_frames::Int64,
+						number_of_bleach_frames::Int64,
+						number_of_postbleach_frames::Int64,
+						file_name_output::String)
 
 	# Inititalization of random number generation device.
 	random_seed::Int64 = convert(Int64, time_ns())
@@ -19,14 +27,12 @@ function run_simulate(	D_SI::Float64,
 	t_start::Int64 = convert(Int64, time_ns())
 
 	# Experimental and simulation parameters.
-	pixel_size::Float64 = 7.5e-7 # m
-	number_of_pixels::Int64 = 64#256 #  pixels
 
-	number_of_prebleach_frames::Int64 = 1
-	number_of_postbleach_frames::Int64 = 1
+	number_of_pixels::Int64 = 256 #  pixels
+
 	delta_t::Float64 = 0.2 # s
 
-	number_of_pad_pixels::Int64 = 2#128 # pixels
+	number_of_pad_pixels::Int64 = 128 # pixels
 	number_of_time_steps_fine_per_course::Int64 = 32
 	number_of_particles::Int64 = 20_000_000_000#40_000_000_000
 
@@ -34,11 +40,6 @@ function run_simulate(	D_SI::Float64,
 	number_of_particles_per_worker::Array{Int64, 1} = convert(Array{Int64, 1}, floor(number_of_particles / number_of_workers) * ones(number_of_workers))
 	number_of_particles_remaining::Int64 = number_of_particles - sum(number_of_particles_per_worker)
 	number_of_particles_per_worker[1:number_of_particles_remaining] .+= 1
-
-	bleach_region_shape::Int64 = 0
-	r_bleach::Float64 = 20.5#15e-6 / pixel_size
-	lx_bleach::Float64 = 20.5#30e-6 / pixel_size
-	ly_bleach::Float64 = 20.5#30e-6 / pixel_size
 
 	D::Float64 = D_SI / pixel_size^2
 
@@ -68,15 +69,9 @@ function run_simulate(	D_SI::Float64,
 	# Measure and print execution time.
 	t_exec::Int64 = convert(Int64, time_ns()) - t_start
 	t_exec_s::Float64 = convert(Float64, t_exec) / 1e9
-	#println(join(("Execution time: ", t_exec_s, " seconds.")))
+	println(join(("Execution time: ", t_exec_s, " seconds.")))
 
 	# Save output.
-	file_name_output::String = file_name_output = "simulated_stochastic_data.bin"
-#	if bleach_region_shape == 0.0
-#		file_name_output = join(("simulated_stochastic_data_circle_", string(D_SI), "_", string(k_on), "_", string(k_off), "_", string(mobile_fraction), "_", string(alpha), "_", string(beta), "_", string(number_of_bleach_frames), ".bin"))
-#	else
-#		file_name_output = join(("simulated_stochastic_data_rectangle_", string(D_SI), "_", string(k_on), "_", string(k_off), "_", string(mobile_fraction), "_", string(alpha), "_", string(beta), "_", string(sigma_bleach), "_", string(number_of_bleach_frames), ".bin"))
-#	end
 	file_stream_output::IOStream = open(file_name_output, "w")
 	write(file_stream_output, D_SI)
 	write(file_stream_output, k_on)
@@ -105,46 +100,55 @@ function run_simulate(	D_SI::Float64,
 	nothing
 end
 
-D_SI = 0.0#5e-11
+# Run simulations.
+pixel_size = 7.5e-7 # m
+number_of_prebleach_frames = 1
+number_of_postbleach_frames = 1
+
+# Small circle, no bleach = 4, D
+D_SI = 1e-11
 k_on = 0.0
 k_off = 1.0
-mobile_fraction = 1.0#0.5#1.0
+mobile_fraction = 1.0
+alpha = 0.9
+beta = 0.998
+gamma = 0.0
+bleach_region_shape = 0
+r_bleach = 15e-6 / pixel_size
+lx_bleach = 20e-6 / pixel_size
+ly_bleach = 20e-6 / pixel_size
+number_of_bleach_frames = 4
+file_name_output = "sim_1.bin"
+run_simulate(D_SI, k_on, k_off, mobile_fraction, alpha, beta, gamma, bleach_region_shape, r_bleach, lx_bleach, ly_bleach, pixel_size, number_of_prebleach_frames, number_of_bleach_frames, number_of_postbleach_frames, file_name_output)
+
+# Large circle, no bleach = 2, DB
+D_SI = 5e-10
+k_on = 1.0
+k_off = 0.5
+mobile_fraction = 1.0
+alpha = 0.8
+beta = 1.0
+gamma = 0.0
+bleach_region_shape = 0
+r_bleach = 25e-6 / pixel_size
+lx_bleach = 20e-6 / pixel_size
+ly_bleach = 20e-6 / pixel_size
+number_of_bleach_frames = 4
+file_name_output = "sim_2.bin"
+run_simulate(D_SI, k_on, k_off, mobile_fraction, alpha, beta, gamma, bleach_region_shape, r_bleach, lx_bleach, ly_bleach, pixel_size, number_of_prebleach_frames, number_of_bleach_frames, number_of_postbleach_frames, file_name_output)
+
+# Large circle, no bleach = 2, DB
+D_SI = 1e-10
+k_on = 0.0
+k_off = 1.0
+mobile_fraction = 0.8
 alpha = 0.7
-beta = 1.0#0.99#1.0
-gamma = 0.0#2.0
+beta = 1.0
+gamma = 2.0
+bleach_region_shape = 1
+r_bleach = 25e-6 / pixel_size
+lx_bleach = 20e-6 / pixel_size
+ly_bleach = 20e-6 / pixel_size
 number_of_bleach_frames = 1
-run_simulate(D_SI, k_on, k_off, mobile_fraction, alpha, beta, gamma, number_of_bleach_frames)
-
-
-#alpha = 0.6
-#
-#t_start = convert(Int64, time_ns())
-#for D_SI in (5e-12, 5e-11, 5e-10)
-#	for k_on in (0.0, 1.0, 10.0)
-#		if k_on == 0.0
-#			k_off = 1.0
-#			for mobile_fraction in (0.8, 1.0)
-#				for beta in (1.0, 0.999, 0.995)
-#					for number_of_bleach_frames in (1, 4)
-#						println((D_SI, k_on, k_off, mobile_fraction, alpha, beta, number_of_bleach_frames))
-#						run_simulate(D_SI, k_on, k_off, mobile_fraction, alpha, beta, number_of_bleach_frames)
-#					end
-#				end
-#			end
-#		else
-#			for k_off in (1.0, 10.0)
-#				for mobile_fraction in (0.8, 1.0)
-#					for beta in (1.0, 0.999, 0.995)
-#						for number_of_bleach_frames in (1, 4)
-#							println((D_SI, k_on, k_off, mobile_fraction, alpha, beta, number_of_bleach_frames))
-#							run_simulate(D_SI, k_on, k_off, mobile_fraction, alpha, beta, number_of_bleach_frames)
-#						end
-#					end
-#				end
-#			end
-#		end
-#	end
-#end
-#t_exec = convert(Int64, time_ns()) - t_start
-#t_exec_s = convert(Float64, t_exec) / 1e9
-#println(join(("Execution time: ", t_exec_s, " seconds.")))
+file_name_output = "sim_3.bin"
+run_simulate(D_SI, k_on, k_off, mobile_fraction, alpha, beta, gamma, bleach_region_shape, r_bleach, lx_bleach, ly_bleach, pixel_size, number_of_prebleach_frames, number_of_bleach_frames, number_of_postbleach_frames, file_name_output)
