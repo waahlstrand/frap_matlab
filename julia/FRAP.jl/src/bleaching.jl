473f614482195c6c388d31b43a91580e1b0cc20f
waahlstrand <vicska@student.chalmers.se>
2020-07-29 10:31:24 +0200

    Reformatted, create bleach mask

--- julia/FRAP.jl/src/bleaching.jl
+++ julia/FRAP.jl/src/bleaching.jl
@@ -1,28 +1,28 @@
-using ImageFiltering: imfilter!, Kernel, centered
-using ImageTransformations: imresize!, interpolate, BSpline, Linear
+using ImageFiltering: imfilter!, imfilter, Kernel, centered
+using ImageTransformations: imresize!, interpolate, BSpline, Linear, Constant
 using Interpolations: interpolate
-
-struct ROI{T}
+using Flux: SamePad, MeanPool
+struct ROI
     shape::String
-    x::T
-    y::T
-    r::Union{Missing,T}
-    lx::Union{Missing,T}
-    ly::Union{Missing,T}
+    x::Int64
+    y::Int64
+    r::Union{Missing,Float64}
+    lx::Union{Missing,Float64}
+    ly::Union{Missing,Float64}
 
     function ROI(shape::String,
-                 x::T,
-                 y::T,
-                 r::Union{Missing,T},
-                 lx::Union{Missing,T},
-                 ly::Union{Missing,T}) where T
+                 x::Int64,
+                 y::Int64,
+                 r::Union{Missing,Float64},
+                 lx::Union{Missing,Float64},
+                 ly::Union{Missing,Float64})
 
         if shape == "circle"
-            new{T}(shape, x, y, r, missing, missing)
+            new(shape, x, y, r, missing, missing)
         elseif shape == "rectangle"
-            new{T}(shape, x, y, missing, lx, ly)
+            new(shape, x, y, missing, lx, ly)
         else
-            throw(ArgumentError(shape, "Field 'shape' must be either 'circle' or 'rectangle'".))
+            #throw(ArgumentError(shape, "Field 'shape' must be either 'circle' or 'rectangle'".))
         end
     end # function
 
@@ -52,57 +52,110 @@ function create_bleach_mask(α::Float64,
                             n_pad_pixels::Int64,
                             bleach_region::ROI)
 
-    upsampling_factor = 15.0; % Needs to be a multiple of 3 due the 'box' method in imresize.
+    upsampling_factor = 15; # Needs to be a multiple of 3 due the 'box' method in imresize.
 
-    if bleach_region.shape == "circle"
-        lb_x = floor(0.5 + bleach_region.x - bleach_region.r - 8 * γ)
-        ub_x = ceil(0.5 + bleach_region.x + bleach_region.r + 8 * γ)
-        lb_y = floor(0.5 + bleach_region.y - bleach_region.r - 8 * γ)
-        ub_y = ceil(0.5 + bleach_region.y + bleach_region.r + 8 * γ)
-    elseif bleach_region.shape == "rectangle"
-        lb_x = floor(0.5 + bleach_region.x - 0.5 * bleach_region.lx - 8 * γ)
-        ub_x = ceil(0.5 + bleach_region.x + 0.5 * bleach_region.lx + 8 * γ)
-        lb_y = floor(0.5 + bleach_region.y - 0.5 * bleach_region.ly - 8 * γ)
-        ub_y = ceil(0.5 + bleach_region.y + 0.5 * bleach_region.ly + 8 * γ)
-    else
-        throw(ArgumentError(bleach_region, "Field 'shape' must be either 'circle' or 'rectangle'".))
-    end
+    lb_x, lb_y, ub_x, ub_y = get_bounds(bleach_region, γ)
 
-    lb_x = lb_x - 1
-    ub_x = ub_x + 1
-    lb_y = lb_y - 1
-    ub_y = ub_y + 1
+    x = range(lb_x-1, stop=ub_x, length=upsampling_factor*(ub_x-lb_x+1))
+    y = range(lb_y-1, stop=ub_y, length=upsampling_factor*(ub_y-lb_y+1))
 
-    #xx = linspace(lb_x - 1, ub_x, upsampling_factor * (ub_x - lb_x + 1));
-    #yy = linspace(lb_y - 1, ub_y, upsampling_factor * (ub_y - lb_y + 1));
-
-    x = range(-n_pixels ÷ 2, length=n_pixels)
-    y = range(-n_pixels ÷ 2, length=n_pixels)
-
-    # List comprehensions are very quick
-    X = [j for i in x, j in y]
-    Y = [i for i in x, j in y]
+    X, Y = meshgrid(x, y)
 
     small_mask = ones(size(X))
+    inds = get_bleach_region_index(bleach_region, X, Y)
 
+    small_mask[inds] .= α
     if γ > 0.0
         σ = upsampling_factor * γ
-        ℓ = 4 * ceil(2 * upsampling_factor * γ) + 1
+        ℓ = convert(Int64, 4 * ceil(2 * upsampling_factor * γ) + 1 )
 
 
-        kernel = centered(Kernel.gaussian(σ, ℓ))
+        #kernel = centered(Kernel.gaussian((σ,), (ℓ,)))
 
-
-        small_mask = imfilter!(small_mask, kernel, "replicate")
+        #small_mask = imfilter(small_mask, kernel, "replicate")
     end
 
+    pool = MeanPool((upsampling_factor, upsampling_factor); pad=0, stride=(upsampling_factor, upsampling_factor))
+    resized_mask = pool(reshape(small_mask, (size(small_mask)..., 1, 1)))
+
+    dims = (max(size(resized_mask)...),max(size(resized_mask)...))
+    resized_mask = reshape(resized_mask, dims)
     # Use multi-bilinear interpolation
-    interpolation = interpolate(small_mask, BSpline(Linear()))
+    #small_mask_size = *(size(small_mask)...)
+    #interpolation = interpolate(small_mask, BSpline(Linear()))
 
     # Pre-allocate the new resized mask (necessary due to non-mature package if we want to
     # specify the interpolation ourselves). Currently identical to
     # imresize(small_mask, (ub_x - lb_x + 1, ub_y - lb_y +1))
-    resized_mask = similar(small_mask, (ub_x - lb_x + 1, ub_y - lb_y +1))
-    resized_mask = imresize!(resized_mask, interpolate(small_mask, BSpline(Linear())))
+    #resized_mask = similar(small_mask, (ub_x -  lb_x + 1, ub_y - lb_y +1))
+    #resized_mask = imresize!(resized_mask, interpolation)
+
+    mask    = ones((n_pixels + 2*n_pad_pixels, n_pixels + 2*n_pad_pixels))
+    mask[n_pad_pixels+lb_x:n_pad_pixels+ub_x, n_pad_pixels+lb_y:n_pad_pixels+ub_y] .= resized_mask
+
+    return mask
 
 end
+
+function get_bounds(bleach_region::ROI, γ::Float64)
+
+    factor = 8
+
+    if bleach_region.shape == "circle"
+
+        lb_x = floor(0.5 + bleach_region.x - bleach_region.r - factor*γ)
+        ub_x = ceil(0.5 + bleach_region.x + bleach_region.r + factor*γ)
+        lb_y = floor(0.5 + bleach_region.y - bleach_region.r - factor*γ)
+        ub_y = ceil(0.5 + bleach_region.y + bleach_region.r + factor*γ)
+
+    elseif bleach_region.shape == "rectangle"
+
+        lb_x = floor(0.5 + bleach_region.x - 0.5 * bleach_region.lx - factor*γ)
+        ub_x = ceil(0.5 + bleach_region.x + 0.5 * bleach_region.lx + factor*γ)
+        lb_y = floor(0.5 + bleach_region.y - 0.5 * bleach_region.ly - factor*γ)
+        ub_y = ceil(0.5 + bleach_region.y + 0.5 * bleach_region.ly + factor*γ)
+    else
+        #throw(ArgumentError(bleach_region, "Field 'shape' must be either 'circle' or 'rectangle'".))
+    end
+
+    lb_x, lb_y, ub_x, ub_y = map(x -> convert(Int64, x), (lb_x, lb_y, ub_x, ub_y))
+
+    lb_x -= 1
+    lb_y -= 1
+    ub_x += 1
+    ub_y += 1
+
+    return lb_x, lb_y, ub_x, ub_y
+end
+
+function get_bleach_region_index(bleach_region::ROI,
+                                 X::Array{Float64, 2},
+                                 Y::Array{Float64, 2})
+
+    if bleach_region.shape == "circle"
+
+        # Make a circular cutout of a matrix
+        idx_bleach = (( X .- bleach_region.x ).^2 + ( Y .- bleach_region.y ).^2
+                        .<= bleach_region.r^2)
+
+    elseif bleach_region.shape == "rectangle"
+
+        # Make a rectangular cutout of a matrix
+        idx_bleach = ((X .>= bleach_region.x .- 0.5 .* bleach_region.lx) .&
+                      (X .<= bleach_region.x .+ 0.5 .* bleach_region.lx) .&
+                      (Y .>= bleach_region.y .- 0.5 .* bleach_region.ly) .&
+                      (Y .<= bleach_region.y .+ 0.5 .* bleach_region.ly))
+    end
+
+    return idx_bleach
+end
+
+
+function meshgrid(x, y)
+
+    # List comprehensions are very quick
+    X = [i for i in x, j in y]
+    Y = [j for i in x, j in y]
+
+    return X, Y
+end # function
--- julia/Main.jl
+++ julia/Main.jl
@@ -17,16 +17,16 @@ function main()
     δt::Float32 = 0.1
     masks = nothing
 
-    α = 0.1
+    α = 0.6
     β = 1.0
-    γ = 0.0
+    γ = 2.0
 
     const n_pixels     = 256
     const n_pad_pixels = 128
     const n_prebleach_frames    = 10
     const n_bleach_frames       = 1
     const n_postbleach_frames   = 100
-    const n_frames              = n_prebleach_frames + n_postbleach_frames 
+    const n_frames              = n_prebleach_frames + n_postbleach_frames
 
     dims         = (n_pixels + 2 * n_pad_pixels, n_pixels + 2 * n_pad_pixels, n_frames)
 
@@ -35,11 +35,11 @@ function main()
     postbleach  = n_prebleach_frames+n_bleach_frames:n_frames
 
     C = Concentration(c₀, ϕₘ, dims)
-    
+
     imaging_mask = create_imaging_bleach_mask(β, n_pixels, n_pad_pixels)
 
     stages = (prebleach, bleach, postbleach)
-    
+
 
     for stage in [prebleach, bleach, postbleach]
 
